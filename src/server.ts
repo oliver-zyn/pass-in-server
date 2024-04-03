d45cd4be03adf85a86ed132f707b596e4edcce49
oliver-zyn <olivermayer15@gmail.com>
2024-04-03 15:55:10 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import fastity from "fastify"
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import { generateSlug } from "./utils/generate-slug"
+
+const app = fastity()
+
+const prisma = new PrismaClient({
+  log: ['query',]
+})
+
+app.post('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  })
+
+  const {
+    title,
+    details,
+    maximumAttendees
+  } = createEventSchema.parse(request.body)
+
+  const slug = generateSlug(title)
+
+  const eventWithSameSlug = await prisma.event.findUnique({
+    where: {
+      slug
+    }
+  })
+
+  if (eventWithSameSlug !== null) {
+    throw new Error('Another event with same title alredy exists.')
+  }
+
+  const event = await prisma.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug,
+    }
+  })
+
+  return reply.status(201).send({ eventId: event.id })
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
