d1e96712ccd92f2b4e113004646dfcf4fd17187b
oliver-zyn <olivermayer15@gmail.com>
2024-04-04 16:00:29 -0300

    finalizando api pass in

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -6,6 +6,8 @@ import { prisma } from "../lib/prisma";
 export async function getEventAttendees(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/events/:eventId/attendees', {
     schema: {
+      summary: 'Get event attendees',
+      tags: ['events'],
       params: z.object({
         eventId: z.string().uuid()
       }),
